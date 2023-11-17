bcc5c3c3e56a329a96ee5dff2400c5eb04eef131
ChrisHwangKS <150236089+ChrisHwangKS@users.noreply.github.com>
2023-11-17 19:55:15 +0900

    InputLayer

---
+++ Assets/Scripts/SettingPanel.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SettingPanel : MonoBehaviour
+{
+    /// <summary>
+    /// 항목을 입력받을 InputField 컴포넌트들을 나타냅니다
+    /// </summary>
+    public TMP_InputField[] m_InputFields;
+
+}
